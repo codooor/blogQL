aee5aee22cde3307c4f7646353535e6e2a892bc8
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-07-17 14:29:17 -0400

    separation of concerns

---
+++ client/src/components/AdminLogin.jsx
@@ -0,0 +1,30 @@
+export default function AdminLogin() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center m-4">
+      <form action="submit">
+        <fieldset>
+          <legend>Admin Login</legend>
+          <div className="form-group">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="form-control"
+            />
+          </div>
+          <div className="text-center">
+            <button className="btn btn-primary">Access Page</button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
