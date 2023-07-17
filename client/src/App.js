20e71a78d5f2852a5cf277d47bc7a78867e7620b
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-07-17 15:59:13 -0400

    creating http link

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Posts from "./pages/Posts";
 import Profile from "./pages/Profile";
+import { AUTH_TOKEN } from "./components/constants.js";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -18,8 +25,22 @@ const cache = new InMemoryCache({
   },
 });
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:5000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache,
 });
 
