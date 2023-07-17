c61537a9f95a93e26e53d7c1677ffbf2a716c1ca
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-07-17 15:39:45 -0400

    setting up admin role

---
+++ server/models/Admin.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+
+export default Admin;
